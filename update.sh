7206d81f86255262ee8ce05f87a62906d79edac7
5Z <76208286+Zdn1235@users.noreply.github.com>
2024-06-18 12:15:03 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -28,7 +28,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/zdnazd/hhh/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/zidnyzd/hhh/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
